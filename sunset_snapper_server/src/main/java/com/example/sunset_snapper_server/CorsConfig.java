3c550f318916642e731fd39d35e2837e35a543f8
shawnnws <shawnngweisong@gmail.com>
2023-09-13 21:54:03 +0800

    remove unused imports

--- sunset_snapper_server/src/main/java/com/example/sunset_snapper_server/CorsConfig.java
+++ sunset_snapper_server/src/main/java/com/example/sunset_snapper_server/CorsConfig.java
@@ -2,11 +2,9 @@ package com.example.sunset_snapper_server;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig {
 
